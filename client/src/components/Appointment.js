07c9ab08ea95811a2e4e886b8bde308b9c762f62
EAnthonycarranza <129874785+EAnthonycarranza@users.noreply.github.com>
2024-03-04 11:34:11 -0600

    Add files via upload

--- client/src/components/Appointment.js
+++ client/src/components/Appointment.js
@@ -78,47 +78,47 @@ const Appointment = () => {
             <button className="btn btn-primary py-3 px-5" onClick={handleQuoteRequest}>Schedule and inspection</button>
           </div>
           <div className="col-lg-8">
-            <div className="bg-light text-center p-5">
-              <form>
-                <div className="row g-3">
-                  <div className="col-12 col-sm-6">
-                    <input type="text" className="form-control border-0" placeholder="Your Name" style={{ height: '55px' }} />
-                  </div>
-                  <div className="col-12 col-sm-6">
-                    <input type="email" className="form-control border-0" placeholder="Your Email" style={{ height: '55px' }} />
-                  </div>
-                  <div className="col-12 col-sm-6">
-                  <DatePicker
-                    selected={startDate}
-                    onChange={(date) => setStartDate(date)}
-                    className="form-control border-0"
-                    placeholderText="Call Back Date"
-                    style={{ height: '55px' }}
-                  />
-                </div>
-                <div className="col-12 col-sm-6">
-                  <DatePicker
-                    selected={startTime}
-                    onChange={(time) => setStartTime(time)}
-                    className="form-control border-0"
-                    placeholderText="Call Back Time"
-                    showTimeSelect
-                    showTimeSelectOnly
-                    timeIntervals={15}
-                    timeCaption="Time"
-                    dateFormat="h:mm aa"
-                    style={{ height: '55px' }}
-                  />
-                </div>
-                  <div className="col-12">
-                    <textarea className="form-control border-0" rows="5" placeholder="Message"></textarea>
-                  </div>
-                  <div className="col-12">
-                    <button className="btn btn-primary py-3 px-5" type="button" onClick={handleQuoteRequest}>Schedule an inspection</button>
-                  </div>
-                </div>
-              </form>
-            </div>
+<div className="bg-light text-center p-5">
+  <form onSubmit={handleQuoteRequest}> {/* Use onSubmit here */}
+    <div className="row g-3">
+      <div className="col-12 col-sm-6">
+        <input type="text" name="name" className="form-control border-0" placeholder="Your Name" style={{ height: '55px' }} />
+      </div>
+      <div className="col-12 col-sm-6">
+        <input type="email" name="email" className="form-control border-0" placeholder="Your Email" style={{ height: '55px' }} />
+      </div>
+      <div className="col-12 col-sm-6">
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          className="form-control border-0"
+          placeholderText="Call Back Date"
+          style={{ height: '55px' }}
+        />
+      </div>
+      <div className="col-12 col-sm-6">
+        <DatePicker
+          selected={startTime}
+          onChange={(time) => setStartTime(time)}
+          className="form-control border-0"
+          placeholderText="Call Back Time"
+          showTimeSelect
+          showTimeSelectOnly
+          timeIntervals={15}
+          timeCaption="Time"
+          dateFormat="h:mm aa"
+          style={{ height: '55px' }}
+        />
+      </div>
+      <div className="col-12">
+        <textarea name="message" className="form-control border-0" rows="5" placeholder="Message"></textarea>
+      </div>
+      <div className="col-12">
+        <button type="submit" className="btn btn-primary py-3 px-5">Schedule an inspection</button> {/* Ensure type="submit" */}
+      </div>
+    </div>
+  </form>
+</div>
           </div>
         </div>
       </div>
