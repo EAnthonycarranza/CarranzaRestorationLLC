8aac7982d88013b0b7ba4b6aaadf8089e94f90a0
Anthony Carranza <129874785+EAnthonycarranza@users.noreply.github.com>
2024-03-26 17:45:14 -0500

    Update Portfolio.js

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,35 +1,20 @@
-// FileName: Portfolio.js
-import React, { useEffect } from 'react';
-
+// FileName: Portfolio.jsx
+import React from 'react';
+import { ElfsightWidget } from 'react-elfsight-widget';
 const Portfolio = () => {
-  useEffect(() => {
-    // Create script element
-    const script = document.createElement('script');
-    script.src = "https://static.elfsight.com/platform/platform.js";
-    script.async = true;
-    script.defer = true;
-    // Append script to the body
-    document.body.appendChild(script);
-
-    // Cleanup the script when the component unmounts
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []); // Empty array ensures this effect runs only once
-
   return (
     <div className="portfolio-wrapper" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
       <div className="container-fluid bg-light py-6 px-5">
         <div className="text-center mx-auto mb-5" style={{ maxWidth: '600px' }}>
           <h1 className="display-5 text-uppercase mb-4">See Our <span className="text-primary">TOP-PICKED</span> Amazing Projects</h1>
         </div>
-
-        <div className="elfsight-app-8a9461ed-6b26-46b1-81bb-899d191c5ecc" data-elfsight-app-lazy></div>
-        <div className="elfsight-app-2f3d3eac-9a2e-4906-b699-0163e5e24d68" data-elfsight-app-lazy></div>
+        <ElfsightWidget widgetID="8a9461ed-6b26-46b1-81bb-899d191c5ecc" />
+        <ElfsightWidget widgetID="2f3d3eac-9a2e-4906-b699-0163e5e24d68" />
       </div>
     </div>
   );
 }
 
 export default Portfolio;
+
 
