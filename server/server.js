40725cd641a94be035bd0a3c3a4642f417d22191
Anthony Carranza <129874785+EAnthonycarranza@users.noreply.github.com>
2024-04-01 00:25:42 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,15 @@ const axios = require('axios');
   });
 
 const app = express();
+
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https' && process.env.NODE_ENV === 'production') {
+    res.redirect(`https://${req.header('host')}${req.url}`);
+  } else {
+    next();
+  }
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/build')));
