0aefb254f691a42bc7060456c67663d1b78435cf
Anthony Carranza <129874785+EAnthonycarranza@users.noreply.github.com>
2024-04-13 19:32:41 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect } from 'react'; // Import useEffect here as well
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'; // Import useLocation
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './App.css';
+import './components/Dashboard';
 import Topbar from './components/Topbar';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
@@ -18,11 +20,19 @@ import ProjectPage from './components/ProjectPage'; // Import ProjectPage
 import PaymentFormComponent from './components/PaymentPage';
 import Portfolio2 from './components/Portfolio-2';
 import FormPage from './components/FormPage';
+import Blog from './components/BlogPage';
+import Detail from './components/Detail';
+import AdminDashboard from './components/AdminDashboard';
 import TypingEffect from './components/TypingEffect';
 //import TeamPage from './components/TeamPage'; // Import ProjectPage
 import TestimonialPage from './components/TestimonialPage'; // Import ProjectPage
 import ContactPage from './components/ContactPage'; // Import ProjectPage
-
+import BlogCreatorPage from './components/BlogCreatorPage';
+import AdminEditBlog from './components/AdminEditBlog';
+import Login from './components/LoginPage';
+import Register from './components/Register';
+import AuthSuccess from './components/AuthSuccess'; // Adjust the path as necessary
+import Dashboard from './components/DashboardPage';
 
 const ScrollToTop = ({ children }) => {
   const location = useLocation(); // Now useLocation is defined
@@ -35,49 +45,55 @@ const ScrollToTop = ({ children }) => {
 };
 
 function App() {
-  const appointmentRef = useRef(null); // Moved inside App component
+  const appointmentRef = useRef(null);
 
-  const scrollToAppointment = () => { // Moved inside App component
-    if(appointmentRef.current) {
+  const scrollToAppointment = () => {
+    if (appointmentRef.current) {
       appointmentRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
   return (
     <Router>
-      <div className="App">
-        <Topbar />
-        <Navbar />
-        <Review /> 
-        <ScrollToTop>
-
-        <Routes>
-          <Route path="/about" element={<AboutPage />}/>
-          <Route path="/services" element={<ServicePage />} />
-          <Route path="/project" element={<ProjectPage />} />
-          <Route path="/form" element={<FormPage />}/>
-          {/*<Route path="/team" element={<TeamPage />} /> */}
-          <Route path="/testimonial" element={<TestimonialPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          {/*<Route path="/pay" element={<PaymentFormComponent />} />*/}
-          <Route path="/" element={
-            <>
-              <Carousel scrollToAppointment={scrollToAppointment} />
-              <About />
-              <Portfolio2 />
-              <Services />
-              <div ref={appointmentRef}><Appointment /></div>
-              <Portfolio />
-              {/*<Team /> */}
-              <Testimonial />
-              {/*<Blog /> */}
-            </>
-          } />
-        </Routes>
-        </ScrollToTop>
-        <Footer />
-        <BackToTopButton />
-      </div>
+      <GoogleOAuthProvider clientId="30495169830-n8gedt6t0sl5b7v6ur0t9hkrn4j2uako.apps.googleusercontent.com">
+        <div className="App">
+          <Topbar />
+          <Navbar />
+          <Review />
+          <ScrollToTop>
+            <Routes>
+            <Route path="/dashboard" element={<Dashboard/>} />
+              <Route path="/AdminEditPage/:id" element={<AdminEditBlog />} />
+              <Route path="/admindashboard" element={<AdminDashboard />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/blog/:id" element={<Detail />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/services" element={<ServicePage />} />
+              <Route path="/project" element={<ProjectPage />} />
+              <Route path="/form" element={<FormPage />} />
+              <Route path="/createblog" element={<BlogCreatorPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/testimonial" element={<TestimonialPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/auth-success" element={<AuthSuccess />} />
+              <Route path="/" element={
+                <>
+                  <Carousel scrollToAppointment={scrollToAppointment} />
+                  <About />
+                  <Portfolio2 />
+                  <Services />
+                  <div ref={appointmentRef}><Appointment /></div>
+                  <Portfolio />
+                  <Testimonial />
+                </>
+              } />
+            </Routes>
+          </ScrollToTop>
+          <Footer />
+          <BackToTopButton />
+        </div>
+      </GoogleOAuthProvider>
     </Router>
   );
 }
