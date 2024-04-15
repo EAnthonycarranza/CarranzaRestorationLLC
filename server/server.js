9e878431559b449485c02aab9fe21c5d8cee978f
Anthony Carranza <129874785+EAnthonycarranza@users.noreply.github.com>
2024-04-14 23:32:35 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -62,6 +62,7 @@ const generateToken = (user) => {
   });
 
 const app = express();
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use((req, res, next) => {
   if (req.header('x-forwarded-proto') !== 'https') {
     res.redirect(301, `https://${req.header('host')}${req.url}`);
@@ -1013,14 +1014,9 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-
-app.use(express.static(path.join(__dirname, '../client/build')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
-
-
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
