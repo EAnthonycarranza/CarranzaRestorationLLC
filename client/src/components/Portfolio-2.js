10d0c57023bcae76a89fb928d434be8f75e21d1b
Anthony Carranza <129874785+EAnthonycarranza@users.noreply.github.com>
2024-04-18 01:19:11 -0500

    Add files via upload

--- client/src/components/Portfolio-2.js
+++ client/src/components/Portfolio-2.js
@@ -1,16 +1,45 @@
-// FileName: Portfolio.jsx
-import React from 'react';
-import { ElfsightWidget } from 'react-elfsight-widget';
-const Portfolio2 = () => {
+import React, { useEffect } from 'react';
+
+export default function Portfolio2() {
+  useEffect(() => {
+    const logos = document.querySelector('.logos');
+    const originalSlide = document.querySelector('.logos-slide');
+    const copy = originalSlide.cloneNode(true);
+    logos.appendChild(copy);
+  }, []);
+
+  const imageBaseURL = 'https://storage.googleapis.com/carpublic';
+  const links = [
+    'https://iicrc.org/iicrccertifiedfirm/',
+    'https://www.contractorconnection.com/about/',
+    'https://www.nrca.net/',
+    'https://www.homeadvisor.com/rated.CarranzaRestoration.71226245.html',
+    '', // No link for image 5
+    'https://www.owenscorning.com/en-us/roofing/contractors/contractor-profile/229456',
+    'https://www.bbb.org/all/bbb-accreditation-standards',
+    'https://www.angi.com/companylist/us/tx/schertz/carranza-restoration-llc-reviews-9611706.htm'
+  ];
+  const descriptions = [
+    'IICRC® Certified Firm',
+    'Contractor Connection®',
+    'National Roofing Contractors Association®',
+    'Home Advisor® Elite Service',
+    'FRW® General Contactors',
+    'Owens Corning® - Contractor',
+    'BBB® Accreditation Business',
+    'Angi® - 2019 Super Service Award'
+  ];
+
   return (
-    <div className="portfolio-wrapper" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <div className="container-fluid">
-        <div className="text-center">
-        </div>
-        <ElfsightWidget widgetID="2f3d3eac-9a2e-4906-b699-0163e5e24d68" />
+    <div className="logos">
+      <div className="logos-slide">
+        {links.map((link, index) => (
+          <a href={link || '#'} key={index} target="_blank" rel="noopener noreferrer">
+            <img src={`${imageBaseURL}/${index + 1}.png`} alt={`Logo ${index + 1}`} />
+            <div>{descriptions[index]}</div>
+          </a>
+        ))}
       </div>
     </div>
   );
 }
-
-export default Portfolio2;
