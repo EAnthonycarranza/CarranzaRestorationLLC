d4f9e22d3a8efe2d52264ba4da3148f01ad49ddb
Anthony Carranza <129874785+EAnthonycarranza@users.noreply.github.com>
2024-05-08 11:31:36 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1336,9 +1336,22 @@ app.get('/contacts', async (req, res) => {
   }
 });
 
+const googleCloudKey = {
+  type: process.env.GOOGLE_CLOUD_TYPE,
+  project_id: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_CLOUD_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLOUD_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_CLOUD_AUTH_URI,
+  token_uri: process.env.GOOGLE_CLOUD_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_CLOUD_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLOUD_CLIENT_X509_CERT_URL
+};
+
 const storage = new Storage({
   projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
-  keyFilename: process.env.GOOGLE_CLOUD_KEYFILE
+  keyFilename: googleCloudKey
 });
 
 const bucket = storage.bucket(process.env.BUCKET_NAME);
