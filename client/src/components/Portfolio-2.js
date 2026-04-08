6abab7e8594734a2c1a2d4b16388e30613759492
Eanthonycarranza <anthonycarranza123@gmail.com>
2026-04-08 12:44:16 -0500

    Update Portfolio-2 partner logos to use local assets from Footer

--- client/src/components/Portfolio-2.js
+++ client/src/components/Portfolio-2.js
@@ -1,42 +1,61 @@
 import React, { useEffect } from 'react';
+import AngleImage from '../img/AngieListimg.png';
+import EliteImage from '../img/EliteServiceimg.png';
+import FWRImage from '../img/FWRimg.png';
+import OwenImage from '../img/OwensCorningimg.png';
+import BImage from '../img/BBBimg.png';
+import NRCA from '../img/NRCA.png';
 
 export default function Portfolio2() {
   useEffect(() => {
     const logos = document.querySelector('.logos');
     const originalSlide = document.querySelector('.logos-slide');
-    const copy = originalSlide.cloneNode(true);
-    logos.appendChild(copy);
+    if (logos && originalSlide) {
+      const copy = originalSlide.cloneNode(true);
+      logos.appendChild(copy);
+    }
   }, []);
 
-  const imageBaseURL = 'https://storage.googleapis.com/carpublic';
-  const links = [
-    'https://iicrc.org/iicrccertifiedfirm/',
-    'https://www.contractorconnection.com/about/',
-    'https://www.nrca.net/',
-    'https://www.homeadvisor.com/rated.CarranzaRestoration.71226245.html',
-    '', // No link for image 5
-    'https://www.owenscorning.com/en-us/roofing/contractors/contractor-profile/229456',
-    'https://www.bbb.org/all/bbb-accreditation-standards',
-    'https://www.angi.com/companylist/us/tx/schertz/carranza-restoration-llc-reviews-9611706.htm'
-  ];
-  const descriptions = [
-    'IICRC® Certified Firm',
-    'Contractor Connection®',
-    'National Roofing Contractors Association®',
-    'Home Advisor® Elite Service',
-    'FRW® General Contactors',
-    'Owens Corning® - Contractor',
-    'BBB® Accreditation Business',
-    'Angi® - 2019 Super Service Award'
+  const logosData = [
+    {
+      img: NRCA,
+      link: 'https://www.nrca.net/',
+      description: 'National Roofing Contractors Association®'
+    },
+    {
+      img: EliteImage,
+      link: 'https://www.homeadvisor.com/rated.CarranzaRestoration.71226245.html',
+      description: 'Home Advisor® Elite Service'
+    },
+    {
+      img: FWRImage,
+      link: '',
+      description: 'FRW® General Contactors'
+    },
+    {
+      img: OwenImage,
+      link: 'https://www.owenscorning.com/en-us/roofing/contractors/contractor-profile/229456',
+      description: 'Owens Corning® - Contractor'
+    },
+    {
+      img: BImage,
+      link: 'https://www.bbb.org/all/bbb-accreditation-standards',
+      description: 'BBB® Accreditation Business'
+    },
+    {
+      img: AngleImage,
+      link: 'https://www.angi.com/companylist/us/tx/schertz/carranza-restoration-llc-reviews-9611706.htm',
+      description: 'Angi® - 2019 Super Service Award'
+    }
   ];
 
   return (
     <div className="logos">
       <div className="logos-slide">
-        {links.map((link, index) => (
-          <a href={link || '#'} key={index} target="_blank" rel="noopener noreferrer">
-            <img src={`${imageBaseURL}/${index + 1}.png`} alt={`Logo ${index + 1}`} />
-            <div>{descriptions[index]}</div>
+        {logosData.map((logo, index) => (
+          <a href={logo.link || '#'} key={index} target="_blank" rel="noopener noreferrer">
+            <img src={logo.img} alt={logo.description} />
+            <div>{logo.description}</div>
           </a>
         ))}
       </div>
